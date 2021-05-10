6f9a181b9c7325d7d3ea77aa3a7911977bcea75d
Diego Gonzales <diegogonzales@outlook.com.br>
2021-05-09 23:30:40 -0300

    Part 2 do Tutorial Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,14 +9,21 @@ import { Hero } from '../hero'
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero?: Hero;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
-  
   }
 
 }
